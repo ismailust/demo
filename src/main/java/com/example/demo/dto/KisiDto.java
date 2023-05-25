4545c373ebf181800e5f4317eadb25e539eddeaa
ismailustt <129238482+ismailustt@users.noreply.github.com>
2023-05-25 21:21:38 +0300

    Java SpringBoot DataJPA ve Postgresql

---
+++ src/main/java/com/example/demo/dto/KisiDto.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import com.example.demo.entity.Adres;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.List;
+@Data
+public class KisiDto {
+
+    private Long id;
+    private String adi;
+    private String soyadi;
+    private List<String> adresler;
+}
