cc4607643b5007d6a19bfd80bf1f6ec902c810a5
ismailust <ismailustt@gmail.com>
2024-02-24 00:10:07 +0300

    Swagger Eklendi

--- src/main/java/com/example/demo/controller/KisiController.java
+++ src/main/java/com/example/demo/controller/KisiController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.KisiDto;
 import com.example.demo.service.KisiService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +14,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/kisi")
 @RequiredArgsConstructor
+@Api(value = "Kisi Adres Documentation")
 public class KisiController {
 
     private final KisiService kisiService;
 
     @PostMapping
+    @ApiOperation(value = "Yeni adres kaydet")
     public ResponseEntity<KisiDto> kaydet(@RequestBody KisiDto kisiDto){
         return ResponseEntity.ok(kisiService.save(kisiDto));
     }
 
+
     @GetMapping
+    @ApiOperation(value = "Tumunu listele")
     public ResponseEntity<List<KisiDto>> tumunuListele(){
         return  ResponseEntity.ok(kisiService.getAll());
     }
--- src/main/java/com/example/demo/dto/KisiDto.java
+++ src/main/java/com/example/demo/dto/KisiDto.java
@@ -1,17 +1,55 @@
 package com.example.demo.dto;
 
-import com.example.demo.entity.Adres;
-import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import java.util.List;
 @Data
+@ApiModel(value = "User Api model documentation", description = "Model")
 public class KisiDto {
 
+    @ApiModelProperty(value = "Unique id field of user object")
     private Long id;
+    @ApiModelProperty(value = "Name field of user object")
     private String adi;
+    @ApiModelProperty(value = "Surname field of user object")
     private String soyadi;
+    @ApiModelProperty(value = "Adres field of user object")
     private List<String> adresler;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    public String getSoyadi() {
+        return soyadi;
+    }
+
+    public void setSoyadi(String soyadi) {
+        this.soyadi = soyadi;
+    }
+
+    public List<String> getAdresler() {
+        return adresler;
+    }
+
+    public void setAdresler(List<String> adresler) {
+        this.adresler = adresler;
+    }
+
+
+
 }
